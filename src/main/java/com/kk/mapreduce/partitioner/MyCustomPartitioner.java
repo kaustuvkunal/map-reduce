2d8fb8f326dea84a6d3c0f5bd5770ab0d586c3c5
K Kunal <kaustuv.kunal@gmail.com>
2018-12-05 11:38:31 +0530

    Update MyCustomPartitioner.java

--- src/main/java/com/kk/mapreduce/partitioner/MyCustomPartitioner.java
+++ src/main/java/com/kk/mapreduce/partitioner/MyCustomPartitioner.java
@@ -7,8 +7,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
  * This Class is a Custom Partitioner class to partition key space into 
  * two part based on male and female
  * <p> 
- * @author Kaustuv Kunal
- * @version 1.0
+ *  
  * @since  1.8 
  * 09-Nov-2018
  */
